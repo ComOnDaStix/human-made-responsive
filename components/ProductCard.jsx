957c545192fe2dc76f8e9bb651aa3297520bafaf
Comeron Moussavi <104601506+ComOnDaStix@users.noreply.github.com>
2023-03-31 20:46:49 -0500

    new new

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
 import styles from "../styles/ProductCard.module.css";
 
-
 const ProductCard = ({ product }) => {
   return (
     <div className={styles.ProductCardContainer}>
       <div className={styles.ImageWrapper}>
         <Image
-          className={styles.ProductImage} 
+          className={styles.ProductImage}
           src={product.imageUrl}
           width={320}
           height={320}
